18417ee106ab92c91e8e26d0150d5388ce3571e4
md-zeon <zeon.cse@gmail.com>
2026-06-28 10:56:13 +0600

    feat: implement ThemeProvider component for theme management

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/sonner";
-import { ThemeProvider } from "./provider/theme-provider";
+import { ThemeProvider } from "../provider/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
